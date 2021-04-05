fea9ba08583e1e4111c90ade0910df89e3bb1524
teenastewart <teenastewart@gmail.com>
2021-04-04 19:43:15 -0500

    Updated math to For statement.

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,22 +5,44 @@ import csv
 # Path to collect data from the Resources folder
 budget_csv = os.path.join("Resources", "budget_data.csv")
 
+
 # Reading the CSV
-with open(budget_csv) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter =",")
+with open(budget_csv, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter =',')
+
     
     # skips the header row
     csv_header = next (csvreader)
+    months = 0
+    net_profit = 0
+    yrmax = 0
+    yrmin = 0
+    for row in csvreader:
     # Calculate the number of months in the CSV
-    months = len(list(csvreader))
-    print(months)
-    net_profit = sum(float(row[1]) for row in csvreader)
-    # net_profit = 0
+        months += 1
+        net_profit += int(row[1])
+        net_money = "${:,.2f}".format(net_profit)
+        if int(row[1]) > yrmax:
+            yrmax = int(row[1])
+        elif int(row[1]) < yrmin:
+            yrmin = int(row[1])    
 
-    # for row in csvreader:
-        # net_profit += int(row[1])
-    print(net_profit)
+    yraverage = (net_profit/months)
+    yravmoney = "${:,.2f}".format(yraverage)
+
     # Calculate the net profit
+    #net_profit = sum(int(row[1]) for row in csvreader)
+    print(f"Total Months: {months}")
+    print(f"Total: {net_money}")
+    print(f"Average Change: {yravmoney}")
+    print(f"Greatest Increase in Profits: {yrmax}")
+    print(f"Greatest Decrease in Profits: {yrmin}")
+    # net_profit = 0
+    # for row in csvreader:
+        #print(row)
+        # net_profit += int(row[1])
+    
+    
     # for row in csv.reader(csvfile):
        
         
